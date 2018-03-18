91eda5a0e70daf4c95947f32c35de21c8c5407b5
Michael Pitz <mpitz@gmx.de>
2018-03-18 16:36:20 +0100

    refactored brie

--- GildedRose.cc
+++ GildedRose.cc
@@ -8,20 +8,78 @@ void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (!isSpecialItem(items[i]))
+        Item& item = items[i];
+
+        if (!isSpecialItem(item))
         {
-            updateNormalItem(items[i]);
+            updateNormalItem(item);
+            return;
+        }
+        if (item.name == "Aged Brie")
+        {
+            updateAgedBrie(item);
+            return;
+        }
+
+        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        {
+            if (items[i].quality > 0)
+            {
+                if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                {
+                    items[i].quality = items[i].quality - 1;
+                }
+            }
         }
         else
         {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+            if (items[i].quality < 50)
             {
-                if (items[i].quality > 0)
+                items[i].quality = items[i].quality + 1;
+
+                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                    if (items[i].days_remaining < 11)
                     {
-                        items[i].quality = items[i].quality - 1;
+                        if (items[i].quality < 50)
+                        {
+                            items[i].quality = items[i].quality + 1;
+                        }
                     }
+
+                    if (items[i].days_remaining < 6)
+                    {
+                        if (items[i].quality < 50)
+                        {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        {
+            items[i].days_remaining = items[i].days_remaining - 1;
+        }
+
+        if (items[i].days_remaining < 0)
+        {
+            if (items[i].name != "Aged Brie")
+            {
+                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (items[i].quality > 0)
+                    {
+                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            items[i].quality = items[i].quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    items[i].quality = items[i].quality - items[i].quality;
                 }
             }
             else
@@ -29,58 +87,6 @@ void GildedRose::updateQuality()
                 if (items[i].quality < 50)
                 {
                     items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (items[i].days_remaining < 11)
-                        {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].days_remaining < 6)
-                        {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros")
-            {
-                items[i].days_remaining = items[i].days_remaining - 1;
-            }
-
-            if (items[i].days_remaining < 0)
-            {
-                if (items[i].name != "Aged Brie")
-                {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (items[i].quality > 0)
-                        {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                }
-                else
-                {
-                    if (items[i].quality < 50)
-                    {
-                        items[i].quality = items[i].quality + 1;
-                    }
                 }
             }
         }
@@ -102,17 +108,19 @@ void GildedRose::updateNormalItem(Item& item) const
     }
 }
 
-// void GildedRose::updateNormalItem(Item &item) const {
-//  if (item.quality != 0) {
-//    if (item.days_remaining > 0) {
-//      item.quality -= 1;
-//    } else {
-//      item.quality -= 2;
-//    }
-//  }
-//  item.days_remaining -= 1;
-//}
-//
+void GildedRose::updateAgedBrie(Item& item) const
+{
+    item.days_remaining -= 1;
+    if (item.quality >= 50)
+    {
+        return;
+    }
+
+    item.quality += 1;
+    if (item.days_remaining <= 0)
+        item.quality += 1;
+}
+
 bool GildedRose::isSpecialItem(const Item& item) const
 {
     return item.name == "Aged Brie" || item.name == "Sulfuras, Hand of Ragnaros" ||
--- GildedRose.h
+++ GildedRose.h
@@ -3,23 +3,26 @@
 
 using namespace std;
 
-class Item {
-public:
-  string name;
-  int days_remaining;
-  int quality;
-  Item(string name, int days_remaining, int quality)
-      : name(name), days_remaining(days_remaining), quality(quality) {}
+class Item
+{
+  public:
+    string name;
+    int days_remaining;
+    int quality;
+    Item(string name, int days_remaining, int quality) : name(name), days_remaining(days_remaining), quality(quality) {}
 };
 
-class GildedRose {
-public:
-  vector<Item> &items;
-  GildedRose(vector<Item> &items);
+class GildedRose
+{
+  public:
+    vector<Item>& items;
+    GildedRose(vector<Item>& items);
 
-  void updateQuality();
-private:
+    void updateQuality();
+
+  private:
     bool isSpecialItem(const Item& item) const;
 
-    void updateNormalItem(Item &item) const;
+    void updateNormalItem(Item& item) const;
+    void updateAgedBrie(Item &item) const;
 };
